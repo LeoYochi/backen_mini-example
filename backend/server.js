eb854e1aec7dd2d5582dbb39d14206f34eb5be6c
LeoYochi <nesnesdswii64@gmail.com>
2025-11-08 19:47:53 -0600

    cambio

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 // Conexión a Mongo Atlas
-mongoose.connect("mongodb+srv://LeoYochiEX:1234@cluster0.anaxouh.mongodb.net/registroUsuarios?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.error(err));
-
 
 // Rutas
 app.use("/api/users", usersRouter);
 
 // Puerto
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-
+app.listen(PORT, "0.0.0.0", () => console.log(`Servidor corriendo en puerto ${PORT}`));
